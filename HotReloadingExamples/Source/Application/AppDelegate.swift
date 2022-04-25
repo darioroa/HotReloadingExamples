8803b4400e455eeb9d97997acd86916d0eee71ae
Dario <darioroa6@gmail.com>
2022-04-25 10:25:02 +0200

    Add Catalyst Injection

--- HotReloadingExamples/Source/Application/AppDelegate.swift
+++ HotReloadingExamples/Source/Application/AppDelegate.swift
@@ -11,6 +11,9 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+#if targetEnvironment(macCatalyst)
+        Bundle(path: "/Applications/InjectionIII.app/Contents/Resources/macOSInjection.bundle")!.load()
+#endif
         return true
     }
 }
